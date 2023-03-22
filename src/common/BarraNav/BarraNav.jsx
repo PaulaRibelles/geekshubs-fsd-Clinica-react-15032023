0f523311af3f5330e264e6e0d7ca197de6a674ac
paula <paula_ribe93@hotmail.com>
2023-03-22 23:31:32 +0100

    fix: Add link on navbar to login and register pages

--- src/common/BarraNav/BarraNav.jsx
+++ src/common/BarraNav/BarraNav.jsx
@@ -1,17 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 
 export const BarraNav = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-            <Navbar.Brand href="#home">TrueSmile</Navbar.Brand>
+            <Navbar.Brand as={Link} to='/'>TrueSmile</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
-            <Nav.Link href="#features">Login</Nav.Link>
-            <Nav.Link href="#pricing">Register</Nav.Link>
+            <Nav.Link as={Link} to={'/login'}>Login</Nav.Link>
+            <Nav.Link as={Link} to={'/register'}>Register</Nav.Link>
         </Nav>
         <Nav>
             <Nav.Link href="#deets">User</Nav.Link>
