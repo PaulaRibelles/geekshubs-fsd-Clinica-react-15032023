a401c7cf90b824ef820cec01353c52c94306fa0c
paula <paula_ribe93@hotmail.com>
2023-03-25 17:59:48 +0100

    feat: Created Appointmentcalendar

--- src/pages/Appointment/Appointment.jsx
+++ src/pages/Appointment/Appointment.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import dayjs from 'dayjs';
+import './Home.css';
+import Calendar from 'react-calendar';
+import { InputText } from '../../common/InputText/InputText';
+
 
 export const Appointment = () => {
+
+  const [newDate, setNewDate] = useState(new Date ());
+
+  useEffect(() => {
+    let dateBack = dayjs(newDate).format("dddd DD MMMM YYYY");
+  }, [newDate]);
+
   return (
-    <div>Appointment</div>
+    <div className='AppointDesign'>
+      <h2> Pide una cita</h2>
+        <InputText 
+        className={""}
+        type="text"
+        maxLength={50}
+        placeholder="Explique el motivo de su visita"
+        />
+        <div>
+          <Calendar onChange={setNewDate} value={newDate}/>
+        </div>
+    </div>
   )
 }
