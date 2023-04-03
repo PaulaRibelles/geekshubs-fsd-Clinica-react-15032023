1e9f23ee5903747d5529d2c79771bf02dafe5cd8
paula <paula_ribe93@hotmail.com>
2023-04-03 13:38:56 +0200

    fix:changes local to deploy link

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const root = "https://clinica-truesmile-production.up.railway.app"
+const root = "https://clinica-truesmile-production.up.railway.app"
 
-const root = "http://localhost:3000"
+// const root = "http://localhost:3000"
 
 export const logMe = async (body) => {
     return await axios.post(`${root}/auth/login`, body);
